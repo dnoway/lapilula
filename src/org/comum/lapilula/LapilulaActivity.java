29cebe037989595075918b1717854b7c24366460
Djavan Fagundes <djavanf@gnome.org>
2012-04-24 23:20:34 -0300

    Created the Eclipse/Android folder tree

---
+++ src/org/comum/lapilula/LapilulaActivity.java
@@ -0,0 +1,13 @@
+package org.comum.lapilula;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class LapilulaActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
